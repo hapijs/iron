3442f5690b867ef85143f996dd37d4f23cf9498e
Eran Hammer <eran@hueniverse.com>
2013-01-12 12:05:50 -0800

    Initial commit

---
+++ test/index.js
@@ -0,0 +1,24 @@
+// Load modules
+
+var Chai = require('chai');
+var Iron = process.env.TEST_COV ? require('../lib-cov') : require('../lib');
+
+
+// Declare internals
+
+var internals = {};
+
+
+// Test shortcuts
+
+var expect = Chai.expect;
+
+
+describe('Iron', function () {
+
+    it('should generate an encrypted token', function (done) {
+
+        done();
+    });
+});
+
